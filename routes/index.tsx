0ce41cde8e27f2eb63ed7ce99215ab5eec1158ce
Justin Morrison <57382686+morrisonak@users.noreply.github.com>
2023-12-26 15:49:07 -0600

    Update index.tsx

--- routes/index.tsx
+++ routes/index.tsx
@@ -10,19 +10,29 @@ export default function Home() {
       </h1>
 
       <div class="p-2">
-        {links.map((link, index) => (
-          <div key={index} class="px-4 py-2 mb-4 bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-200">
-            <a 
-              href={link.url} 
-              target="_blank" 
-              rel="noopener noreferrer" 
-              class="text-blue-600 hover:text-blue-800 transition-colors duration-200"
-            >
-              {link.url}
-            </a>
-            {link.label && <p class="font-semibold mt-1">{link.label}</p>}
-          </div>
-        ))}
+        {links.map((link, index) => {
+          // Construct the image path
+          const imagePath = `/images/${link.url.split('/').pop()}.jpg`;
+
+          return (
+            <div key={index} class="px-4 py-2 mb-4 bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-200">
+              {link.url.includes('/t/') && 
+                <img src={imagePath} alt={link.label || 'Image preview'} class="w-full h-40 object-cover rounded-t-lg" />}
+              <div class="p-4">
+                <a 
+                  href={link.url} 
+                  target="_blank" 
+                  rel="noopener noreferrer" 
+                  class="text-blue-600 hover:text-blue-800 transition-colors duration-200"
+                >
+                  {link.url}
+                </a>
+                {link.label && <p class="font-semibold mt-1">{link.label}</p>}
+                <p class="text-gray-600 text-sm">Date Added: {link.dateAdded}</p>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
