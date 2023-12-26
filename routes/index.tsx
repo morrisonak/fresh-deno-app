4fa667386e1b8e18729517d2b7565239b4c65a4b
Justin Morrison <57382686+morrisonak@users.noreply.github.com>
2023-12-26 11:11:25 -0600

    Update index.tsx

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,5 +1,4 @@
 import Header from "../islands/Header.tsx";
-
 import links from "../links.json" assert { type: "json" };
 
 export default function Home() {
@@ -10,13 +9,19 @@ export default function Home() {
         Welcome to `fresh`. Try update this message in the ./routes/index.tsx
         file, and refresh.
       </p>
-     
 
       <div class="p-2">
         {links.map((link, index) => (
-          <div key={index} class="px-2 mb-4">
-            <a href={link.url} target="_blank" rel="noopener noreferrer">{link.url}</a>
-            {link.label && <p class="font-semibold">{link.label}</p>}
+          <div key={index} class="px-4 py-2 mb-4 bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-200">
+            <a 
+              href={link.url} 
+              target="_blank" 
+              rel="noopener noreferrer" 
+              class="text-blue-600 hover:text-blue-800 transition-colors duration-200"
+            >
+              {link.url}
+            </a>
+            {link.label && <p class="font-semibold mt-1">{link.label}</p>}
           </div>
         ))}
       </div>
