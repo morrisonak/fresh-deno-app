3ba732fb09cca28e698796c10c7016da906629fd
Justin Morrison <morrisonak@yahoo.com>
2023-12-26 16:01:35 -0600

    updates

---
+++ autoUp.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Change to the project directory
+cd /home/justice/fresh-deno-app/
+
+git pull
+# Execute the bun command
+/home/justice/.bun/bin/bun run v0.ts
+
+git add static/images/*.jpg
+
+
+# Add links.json to the staging area
+git add links.json
+
+# Commit with a message including the current timestamp
+currentTimeStamp=$(date)
+git commit -m "update json at $currentTimeStamp"
+
+# Push the commit to the remote repository
+git push
+
+# Echo a success message
+echo "Links successfully updated"
+
